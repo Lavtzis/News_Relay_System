e3cf0cdf8e05f17c4c5e319e75dd007d9878f6d0
Lavtzis <lavtzisandreas21@gmail.com>
2026-03-31 08:47:02 +0300

    Cleaned main.py
    Code cleanup and re-organisation to better suit the eye of a human being.

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import os
 import discord
 from discord.ext import commands
+import logging
 from dotenv import load_dotenv
 
 ##### Main Branch   
 
+# dotenv
 load_dotenv()
 token = os.getenv('TOKEN')
 
+# Logging (Debug & Crashes)
+handler = logging.FileHandler(filename='discord_news.log', encoding='utf-8', mode='w')
+
+# Discord Intents
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
+# Command Settings
 bot = commands.Bot(command_prefix='!n', intents=intents)
 
 ##### Bot Run Token -- DONT TOUCH
