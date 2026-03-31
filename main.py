61f63578461d3ca36c2e262a2d5f821f15f30a7f
Lavtzis <lavtzisandreas21@gmail.com>
2026-03-31 08:40:49 +0300

    PyCharm

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 ##### Main Branch   
 
 load_dotenv()
 token = os.getenv('TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='!n', intents=intents)
+
+##### DONT TOUCH
+bot.run(token)
