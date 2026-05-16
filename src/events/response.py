d32a7bd1d850930d3090ed740c088809fc990d91
Lavtzis <lavtzisandreas21@gmail.com>
2026-05-16 17:25:53 +0300

    Update response.py

--- src/events/response.py
+++ src/events/response.py
@@ -2,11 +2,14 @@ import discord
 from discord.ext import commands
 
 # Core
-bot = commands.Bot(command_prefix='!')
-
 # Test file to test communication with a select channel on Discord.
-@bot.command()
-async def hello2(res):
-    await res.send(f"Hello from the response.py file!")
+
+class greetingsResponse(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def hello2(res):
+        await res.send(f"Hello from the response.py file!")
 
 print(f"Hello2 has been loaded successfully")
