e4e38b49114ad65115f8cfea1bcfa39ce30968b0
Lavtzis <lavtzisandreas21@gmail.com>
2026-05-16 18:37:37 +0300

    Update administratorCommands.py

--- src/commands/administratorCommands.py
+++ src/commands/administratorCommands.py
@@ -1,6 +1,5 @@
 # This file handles all commands that can be issued by users with administrator privileges.
 # Allows the termination of the bot through command. (Bot requires manual reactivation afterwards).
 # Allows setting other users as administrators or removing them.
-
 import discord
 from discord.ext import commands
