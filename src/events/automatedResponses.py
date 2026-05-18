91294023191c32ed3f6d5d1fabd23c034eafe32d
Lavtzis <lavtzisandreas21@gmail.com>
2026-05-18 20:58:03 +0300

    Update automatedResponses.py
    Switch from directly using the channel  ID to using the dotenv (Using Channel ID is not secure and generally bad practice)

--- src/events/automatedResponses.py
+++ src/events/automatedResponses.py
@@ -1,6 +1,11 @@
+import os
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 #import datetime as dt
+
+load_dotenv()
+test_channel = int(os.getenv('TEST_CHANNEL'))
 
 # Core
 # All automated responses like the on_ready fire from this file.
@@ -11,11 +16,13 @@ class automatedResponses(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print(f"Automated responses ready: {self.bot.user.name}")
-        channel = self.bot.get_channel(1488399642969374800)
+        channel = self.bot.get_channel(test_channel)
+        await channel.send(f"News Bot is Online.")
+
         #dt.strftime("%d:%m:%Y:%H:%M")
         #time = dt.datetime
         #await channel.send(f"{time} - News Bot is Online.")
-        await channel.send(f"News Bot is Online.")
+        
 
 async def setup(bot):
     await bot.add_cog(automatedResponses(bot))
