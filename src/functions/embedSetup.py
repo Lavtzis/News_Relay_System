3dc1576163a14c8c8ef4832ebdbdfc014658f2d3
Lavtzis <lavtzisandreas21@gmail.com>
2026-05-19 18:49:21 +0300

    Create embedSetup.py
    Created the embedSetup file to handle the structure of the embeds on which the articles will be posted on.

---
+++ src/functions/embedSetup.py
@@ -0,0 +1,12 @@
+# This file handles the construction/building of the embed structure. Changes here would reflect a change in every embed posted afterwards.
+# This function is called automatically when a channel is selected. It is called every time, with all relevant information sent to it. It constructs the embed and then returns it.
+# Currenlty expiremental as I do not know if embeds work like this
+import discord
+from discord.ext import commands
+
+class embedSetup():
+    embedVar = discord.Embed(title="TestTitle")
+
+embedTest = embedSetup
+
+print(embedTest)
