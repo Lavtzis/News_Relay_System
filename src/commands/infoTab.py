6fbd44053f091a174e7f839d31ffe5fce107cb04
Lavtzis <lavtzisandreas21@gmail.com>
2026-05-19 19:33:11 +0300

    Update infoTab.py

--- src/commands/infoTab.py
+++ src/commands/infoTab.py
@@ -12,6 +12,7 @@ class infoTab(commands.Cog):
         embedVar = discord.Embed(title="News Relay System (Bot)", description="This is a Discord system/bot that relays news articles from an external database or feed into a specified Discord channel.")
         embedVar.add_field(name="!botInfo", value="Posts a panel with information about available commands and information relevant to this Bot.", inline=False)
         embedVar.add_field(name="!selectChannel", value="Select a channel by saying the channel ID you wish the bot to post the news articles in.", inline=False)
+        #embedVar.add_field()
         await ctx.channel.send(embed=embedVar)
 
 async def setup(bot):
