f53252c54c886a9bc8c004e9690ccdcf97db07c3
Lavtzis <lavtzisandreas21@gmail.com>
2026-05-19 18:48:50 +0300

    Update administratorCommands.py
    Setup administrator file and first functionality, that being the information panel.

--- src/commands/administratorCommands.py
+++ src/commands/administratorCommands.py
@@ -3,3 +3,22 @@
 # Allows setting other users as administrators or removing them.
 import discord
 from discord.ext import commands
+
+class administratorPanel(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def adminInfo(self, ctx):
+        await ctx.channel.send("This is the administrator panel")
+
+    # Commmands to be added
+    #
+    # - Shutdown command, to shutdown without need to turn off the server (COMPLETE SHUTDOWN)
+    # - Halt command, bot ONLINE but doesn't post news anymore.
+    # - Set Administrators, allows adding administrators who can use the administrator commands.
+    # - Remove Administrators, removing administrators.
+    # - Change rate, change the rate/delay at which articles are posted.
+
+async def setup(bot):
+    await bot.add_cog(administratorPanel(bot))
