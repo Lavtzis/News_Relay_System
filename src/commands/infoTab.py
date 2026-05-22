1d4d1078de796f6e1009316cb556a4788e8efce6
Lavtzis <lavtzisandreas21@gmail.com>
2026-05-22 22:06:36 +0300

    Update infoTab.py
    Renamed variables to be more appropriate.

--- src/commands/infoTab.py
+++ src/commands/infoTab.py
@@ -2,20 +2,20 @@
 import discord
 from discord.ext import commands
 
-class infoTab(commands.Cog):
+class infoCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
     
     @commands.command()
     async def info(self, ctx):
-        embedVar = discord.Embed(title="News Relay System (Bot)", description="This is a Discord system/bot that relays news articles from an external database or feed into a specified Discord channel.")
-        #embedVar.color(0x1ABC9C)
-        embedVar.add_field(name="!botInfo", value="Posts a panel with information about available commands and information relevant to this Bot.", inline=False)
-        embedVar.add_field(name="!selectChannel", value="Select a channel by saying the channel ID you wish the bot to post the news articles in.", inline=False)
-        embedVar.add_field(name="!adminInfo", value="A command for administrators that allows them to check what commands they can use.")
-        await ctx.channel.send(embed=embedVar)
+        em = discord.Embed(title="News Relay System (Bot)", description="This is a Discord system/bot that relays news articles from an external database or feed into a specified Discord channel.")
+        #em.color(0x1ABC9C)
+        em.add_field(name="!botInfo", value="Posts a panel with information about available commands and information relevant to this Bot.", inline=False)
+        em.add_field(name="!selectChannel", value="Select a channel by saying the channel ID you wish the bot to post the news articles in.", inline=False)
+        em.add_field(name="!adminInfo", value="A command for administrators that allows them to check what commands they can use.")
+        await ctx.channel.send(embed=em)
 
 async def setup(bot):
-    await bot.add_cog(infoTab(bot))
+    await bot.add_cog(infoCommands(bot))
 
