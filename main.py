69e005e04041578d1cc3202eaf407bb25415e984
Lavtzis <lavtzisandreas21@gmail.com>
2026-05-22 20:12:24 +0300

    Update main.py
    Removed old unused code. Readability fix

--- main.py
+++ main.py
@@ -29,21 +29,13 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():   
     print(f"system ready: {bot.user.name}")
-
-# Deprecated - Used only for testing - Will be removed once Cog implementation is compplete
-@bot.command()
-async def oldBotInfo(ctx):
-    embedVar = discord.Embed(title="News Relay System (Bot)", description="This is a Discord system/bot that relays news articles from an external database or feed into a specified Discord channel.")
-    embedVar.add_field(name="!botInfo", value="Posts a panel with information about available commands and information relevant to this Bot.", inline=False)
-    embedVar.add_field(name="!selectChannel", value="Select a channel by saying the channel ID you wish the bot to post the news articles in.", inline=False)
-    await ctx.channel.send(embed=embedVar)
-    # Use embeds similarly later on for the news articles. TEST WEBHOOKS
     
 ##### Bot Run Token & Logging -- DONT TOUCH
 async def main():
     await bot.load_extension("src.commands.infoTab")
     await bot.load_extension("src.commands.administratorCommands")
     await bot.load_extension("src.events.automatedResponses")
+    await bot.load_extension("src.functions.embedCreator")
     await bot.start(token)
 
 asyncio.run(main())
