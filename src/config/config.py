380fcca1b9b45ddbba556357f4d45863c2ed9c46
Lavtzis <lavtzisandreas21@gmail.com>
2026-05-22 21:32:54 +0300

    Update config.py

---
+++ src/config/config.py
@@ -0,0 +1,10 @@
+# Configuration file holds data for variables like the embed's color on default posts (info, adminInfo etc.) and other minor variables, that can be changed by users through commands that will be implemented in the future.
+import discord
+from discord.ext import commands
+
+# RSS
+FEED_URLS = []
+POST_INTERVAL = 10 # REMINDER, number represents minutes, similarly to RSS interval.
+
+# Misc
+EMBED_CLR = "" # Unused
